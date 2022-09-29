8c4c928d7e46fdcee7947f5b446e5dc41ad71107
TreTreFab <htrueba2684@bghscloud.org>
2022-09-29 12:27:59 -0700

    almost done
    almost done

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -5,6 +5,30 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+
+        Scanner scanner = new Scanner(System.in);
+
+        double taxRate;
+        double grandTotal;
+        double totalPrice;
+        int numBoards;
+        int numWindows;
+
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = scanner.nextDouble();
+        System.out.println("How many boards do you need? ");
+        numBoards = scanner.nextInt();
+        System.out.println("How many windows do you need? ");
+        numWindows = scanner.nextInt();
+
+        Construction bghsPR3 = new Construction(8,11,taxRate);
+        totalPrice = bghsPR3.lumberCost(numBoards) + bghsPR3.windowCost(numWindows);
+        grandTotal = bghsPR3.grandTotal(totalPrice);
+        System.out.println("Total: " + totalPrice);
+        System.out.println("Total: " + grandTotal);
+
+        scanner.close();
+
+
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -5,6 +5,27 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        Scanner scanner = new Scanner(System.in);
+        double startLatitude;
+        double startLongitude;
+        double endLatitude;
+        double endLongitude;
+
+        System.out.println("Enter the latitude of the starting location: ");
+        startLatitude = scanner.nextDouble();
+        System.out.println("Enter the longitude of the starting location: ");
+        startLongitude = scanner.nextDouble();
+        System.out.println("Enter the latitude of the ending location: ");
+        endLatitude = scanner.nextDouble();
+        System.out.println("Enter the longitude of the ending location: ");
+        endLongitude = scanner.nextDouble();
+
+
+
+        scanner.close();
+
+
+
+
     }
 }
